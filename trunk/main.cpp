e1d4c33bb8a11e2caf0d80100b89fe336577b6d3
storfisken88 <storfisken88@18f944ca-00ec-96f6-dd15-2a63c25df9ff>
2010-09-17 13:39:43 +0000

    hej

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -1,6 +1,27 @@
-
 #include <iostream>
-int main()
+#include "client.hpp"
+using namespace std;
+
+int main(int argc, char ** argv)
 {
-    return 1337;
+	std::string lHost,lPort,lBoard;
+	if(argc==4)
+	{
+		lHost = std::string(argv[1]);
+		lPort = std::string(argv[2]);
+		lBoard = std::string(argv[3]);
+	}
+	else if (argc==2)
+	{
+		lHost = std::string("cvap103.nada.kth.se");
+		lPort = std::string("5555");
+		lBoard = std::string(argv[1]);
+	}
+	else
+	{
+        std::cerr << "Usage: client (<host> <port>) <board2Solve>" << std::endl;
+        return 1;
+	}
+	send(lHost, lPort, lBoard);
+	return 0;
 }
