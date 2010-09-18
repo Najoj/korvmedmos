6636f095e0d255d78166eaea4c2b4b7beb9365ad
jnordgren@gmail.com <jnordgren@gmail.com@18f944ca-00ec-96f6-dd15-2a63c25df9ff>
2010-09-18 16:26:37 +0000

    mer definitions

--- trunk/common.hpp
+++ trunk/common.hpp
@@ -6,6 +6,20 @@
 #ifndef COMMON_HPP
 #define COMMON_HPP
 
+
+/**
+ * Common definitions
+ */
+
+//Defines board elements, see, http://www.sokobano.de/wiki/index.php?title=Level_format
+#define B_ELEM_WALL 35 //#	 0x23
+#define B_ELEM_PLAYER 64 //@	 0x40
+#define B_ELEM_PLAYER_GSQUARE 43  //+	 0x2b
+#define B_ELEM_BOX 36 //$	 0x24
+#define B_ELEM_BOX_GSQUARE 42  //*	 0x2a
+#define B_ELEM_GSQUARE 46 //.	 0x2e
+#define B_ELEM_FLOOR 32 //(Space) 	 0x20
+
 struct Position{
 	int x;
 	int y;
