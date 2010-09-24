edc66968e050ac0319f878ba285a14aa0be09765
thejojan <thejojan@18f944ca-00ec-96f6-dd15-2a63c25df9ff>
2010-09-24 08:09:52 +0000

    "Compile and run"-skript skapat. Innehåller beskrivning för att installera bloomfilter.

---
+++ compileAndRun.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# You need boost bloom filter to run this program.
+# Get code:
+#  svn co https://svn.boost.org/svn/boost/sandbox/bloom_filter/trunk/
+# Install:
+#  Go to trunk/boost/ and copy * to /usr/include/boost/ (recursively) then copy
+#  trunk/boost/bloom_filter/detail/* to /usr/include/boost/detail
+
+# Compile...
+make
+
+# ... and let the fun begin. :-)
+for i in $(seq 1 180); do
+    ./main $i
+done
