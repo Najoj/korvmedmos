ae77600eb6d29a5c7eb05c673cb1a1384d9c0649
thejojan <thejojan@18f944ca-00ec-96f6-dd15-2a63c25df9ff>
2010-09-28 20:51:55 +0000

    La till ett skript för tidsbegränsa hur längre main körs, samt liten ändring i test-skriptet.

--- testScript.bash
+++ testScript.bash
@@ -4,11 +4,14 @@
 # Count number of good solutions with  grep -c Good RUN
 
 # Compile...
-make
+#make
 
 # ... and let the fun begin. :-)
 for i in $(seq 1 136); do
-    echo -ne "$i:\t"
+    echo -ne "=== $i ===\n"
     time ./main $i
+    ps -e | grep main | awk '{print $3}'
     echo -e "\n"
 done
+
+# To make a process kill after given time use timelimit.bash
