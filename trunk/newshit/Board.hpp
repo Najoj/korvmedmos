6066e7a6ea3cc88ba3fdf78c5c24e030089b0aca
storfisken88 <storfisken88@18f944ca-00ec-96f6-dd15-2a63c25df9ff>
2010-10-17 20:30:59 +0000

    89 boards IN YOUR FACE

---
+++ trunk/newshit/Board.hpp
@@ -0,0 +1,138 @@
+/**
+ * Board.hpp
+ *
+ * Representing the static parts of the Sokoban board. Which is the walls, the
+ * floor and the goal positions.
+ */
+
+#ifndef BOARD_HPP
+#define BOARD_HPP
+#include "common.hpp"
+#include <vector>
+#include "node.hpp"
+using namespace std;
+struct Board
+{
+	protected:
+		// This is the original position of Jens.
+		Position jens;
+		
+	private:
+		// Used for representing the entire board.
+		unsigned char ** matrix;
+		
+	public:
+		//This is the original position of the when the game starts
+		vector<Position> goals;
+		// This is the original position of the boxes when the game starts
+		vector<Position> boxes;
+		
+		// Width and height of the board.
+		int width, height;
+		
+		Board() {}
+		Board(int width, int height)
+		{
+			this->width = width;
+			this->height = height;
+			matrix = new unsigned char*[width];
+		//	cost_matrix = new int*[width];
+			for (int i = 0; i < width; i++)
+				matrix[i] = new unsigned char[height];
+		//		cost_matrix[i] = new int[height];
+			//	cost_matrix[i] = 0;
+		}
+		bool xline_has_goal(int x)
+		{
+			for (int i = 0; i < height; i++)
+			{
+				if (get(x, i) == GOAL)
+					return true;
+			}
+			return false;
+		}
+		bool yline_has_goal(int y)
+		{
+			for (int i = 0; i < width; i++)
+			{
+				if (get(i, y) == GOAL)
+					return true;
+			}
+			return false;
+		}
+		void add_goal(Position p)
+		{
+			goals.push_back(p);
+			matrix[p.x][p.y] = GOAL;
+		}
+		int get(int x, int y)
+		{
+			// If we are outside to board.
+			if(x < 0 || width <= x)
+				return WALL;
+			if(y < 0 || height <= y)
+				return WALL;
+			return matrix[x][y];
+		}
+		int get(Position p)
+		{
+			return get(p.x, p.y);
+		}
+		void set(int x, int y, int value)
+		{
+			matrix[x][y] = value;
+		}
+		void set(Position p, int value)
+		{
+			matrix[p.x][p.y] = value;
+		}
+
+		void insert_boxes(Position* boxes,int len)
+		{
+			for(int i = 0; i < len; i++)
+			{
+
+				if(matrix[boxes[i].x][boxes[i].y] == GOAL)
+				{
+					matrix[boxes[i].x][boxes[i].y] = BOX_ONGOAL;
+				//	cout << "BOX_ONGOAL inserted (" << boxes[i].x << "," << boxes[i].y << endl;
+				}
+				else
+				{
+					matrix[boxes[i].x][boxes[i].y] = BOX;
+				//	cout << "BOX inserted (" << boxes[i].x << "," << boxes[i].y << endl;
+				}
+			}
+		}
+		/**
+		 * Denna gör en dum grejj med goals! Nån annan får fixa.
+		 */
+		void remove_boxes(Position* boxes,int len)
+		{
+			for(int i = 0; i < len; i++){
+				if(matrix[boxes[i].x][boxes[i].y] == BOX_ONGOAL)
+				{
+					matrix[boxes[i].x][boxes[i].y] = GOAL;
+				}
+				else
+				{
+					matrix[boxes[i].x][boxes[i].y] = FLOOR;
+				}
+			}
+
+			for(unsigned int i = 0; i<goals.size(); i++){
+				matrix[goals[i].x][goals[i].y] = GOAL;
+			}
+		}
+		
+		Position * getJens()
+		{
+			return &jens;
+		}
+		
+		void setJens(Position p)
+		{
+			jens = p;
+		}
+};
+#endif
---
+++ trunk/newshit/node.hpp
@@ -0,0 +1,177 @@
+/**
+ * node.hpp
+ */
+
+#ifndef NODE_HPP
+#define NODE_HPP
+
+#include <vector>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "common.hpp"
+
+using namespace std;
+
+class Board;
+
+class Node{
+	private:
+		// Position of player, Jens.
+		Position jens;
+		
+		// Used direction of, that is the nodes childrens directions.
+		char used_directions[4];
+		
+		// Length of the board matrix
+		unsigned char len;
+		
+		// Pointer to boxes positions.
+		Position * boxes_positions;
+		
+		// Path cost
+		int path_cost;
+		
+		// Goal cost
+		int goal_cost;
+
+		// Parent of the node
+		Node * parent;
+		
+		Node * getChildDirection(int dir);
+		
+		bool deadlock(Position pos, Position parent);
+
+
+	public:
+		unsigned char LAST_DIR;
+		int moved_box;
+		Node() {};
+		Node(Position jens, Node * parent, Position *boxes, int len, Position dir, int walker) {
+			this->len = len;
+			moved_box = -1;
+			boxes_positions = new Position[len];
+			// Copy boxes.
+			for(int i = 0; i < len; i++)
+			{
+				// Update boxes if needed.
+				if(boxes[i] == jens) {
+					moved_box = i;
+				//	cout << "Uppdaterar boxar!";
+					boxes_positions[i] = boxes[i] + dir;
+				} else {
+					boxes_positions[i] = boxes[i];
+				}
+			}
+
+			// Saves Jens' posistion.
+			this->jens = jens;
+
+			// Set goal cost.
+			this->goal_cost = 0;
+
+			// Sets parent node
+			this->parent = parent;
+
+			this->path_cost = 0;
+
+
+			this->LAST_DIR = walker;
+			if(parent != NULL){
+				this->path_cost = parent->getPathCost()+1;
+			}
+		}
+
+		Node(const Node& n){
+			this->jens = n.jens;
+			this->goal_cost = n.goal_cost;
+			this->path_cost = n.path_cost;
+
+			this->parent = n.parent;
+			this->len = n.len;
+
+			this->boxes_positions = new Position[this->len];
+			for(int i = 0; i < len; i++){
+				this->boxes_positions[i] = n.boxes_positions[i];
+			}
+
+			this->LAST_DIR = n.LAST_DIR;
+		}
+		/**
+		 * Auto genereated getter and setter methods.
+		 */
+		Position * getBoxes()
+		{
+			return boxes_positions;
+		}
+		
+		int getPathCost() {
+			return path_cost;
+		}
+	    int getPathCost() const{
+			return path_cost;
+		}
+	    int getGoalCost(){
+	    	return goal_cost;
+	    }
+	    int getGoalCost() const{
+	    	return goal_cost;
+	    }
+		void move_box(int i, Position p)
+		{
+		    boxes_positions[i] = p;
+		}
+		Position getCurrent_position()
+		{
+			return jens;
+		}
+		
+		void setParent(Node * n ){
+			this->parent = n;
+		}
+		void setCurrent_position(Position jens)
+		{
+			this->jens = jens;
+		}
+		
+		void setGoalCost(int g){
+			this->goal_cost = g;
+		}
+
+
+		Node * getParent() {
+			return parent;
+		}
+		
+		/**
+		 * Hash and hash related stuff. DO NOT TOUCH!
+		 */
+		bool operator==(const Node & other) const
+		{
+			if (this->jens != other.jens)
+				return false;
+			for (int i = 0; i < len; i++)
+			{
+				if (this->boxes_positions[i] != other.boxes_positions[i])
+				return false;
+			}
+			return true;
+		}
+		
+		friend std::size_t hash_value(Node const& p)
+		{
+			std::size_t hash = 0;
+			hash += 499973*p.jens.x + 849731*p.jens.y;
+			for(int i = 0; i < p.len; i++)
+			{
+				hash += (612301*(p.boxes_positions[i].x<<i)) ^ (1182463*p.boxes_positions[i].y);
+			}
+			return hash;
+		}
+
+		int getLen() {
+			return len;
+		}
+};
+#endif
